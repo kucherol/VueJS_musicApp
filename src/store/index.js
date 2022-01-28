f49d52f4c8246d87128d60614229c478d591239c
kucherol <kucherol2108@gmail.com>
2022-01-28 12:50:47 +0200

    login/register modal added

--- src/store/index.js
+++ src/store/index.js
@@ -4,4 +4,12 @@ export default createStore({
   state: {
     authModalShow: false,
   },
+  mutations: {
+    toggleAuthModal: (state) => {
+      state.authModalShow = !state.authModalShow;
+    },
+  },
+  getters: {
+    // authModalShow: (state) => state.authModalShow,
+  },
 });
