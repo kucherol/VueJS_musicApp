8a2f4296e4c296b00de77051498b5be1ebbecf88
kucherol <kucherol2108@gmail.com>
2022-01-31 12:02:37 +0200

    login/register form validation finished

---
+++ src/components/LoginForm.vue
@@ -0,0 +1,63 @@
+<template>
+  <!-- Login Form -->
+  <div class="text-white text-center font-bold p-5 mb-4"
+    v-if="log_show_alert" :class="log_alert_variant">
+    {{ log_alert_msg }}
+  </div>
+  <vee-form :validation-schema="loginSchema"
+    @submit="login" >
+    <!-- Email -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Email</label>
+      <vee-field type="email" name="email"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+          duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Enter Email" />
+        <ErrorMessage class="text-red-600" name="email" />
+    </div>
+    <!-- Password -->
+    <div class="mb-3">
+      <label class="inline-block mb-2">Password</label>
+      <vee-field type="password" name="password"
+        class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition
+          duration-500 focus:outline-none focus:border-black rounded"
+        placeholder="Password" />
+        <ErrorMessage class="text-red-600" name="password" />
+    </div>
+    <button type="submit" :disabled="log_in_submission"
+      class="block w-full bg-purple-600 text-white py-1.5 px-3 rounded transition
+        hover:bg-purple-700">
+      Submit
+    </button>
+  </vee-form>
+</template>
+
+<script>
+export default {
+  name: 'LoginForm',
+  data() {
+    return {
+      loginSchema: {
+        email: 'required|min:3|max:100|email',
+        password: 'required|min:3|max:100',
+      },
+      log_in_submission: false,
+      log_show_alert: false,
+      log_alert_variant: 'bg-blue-500',
+      log_alert_msg: 'Please wait a second.',
+    };
+  },
+  methods: {
+    login(values) {
+      this.log_show_alert = true;
+      this.log_in_submission = true;
+      this.log_alert_variant = 'bg_blue-500';
+      this.log_alert_msg = 'Please wait, your account is being created.';
+
+      this.log_alert_variant = 'bg-green-500';
+      this.log_alert_msg = 'Success!!!';
+      console.log(values);
+    },
+  },
+};
+</script>
--- src/includes/validation.js
+++ src/includes/validation.js
@@ -3,12 +3,14 @@ import {
   Field as VeeField,
   defineRule,
   ErrorMessage,
+  configure,
 } from 'vee-validate';
 import {
   required, min, max, alpha_spaces as alphaSpaces, email,
   min_value as minVal,
   max_value as maxVal,
   confirmed,
+  not_one_of as excluded,
 } from '@vee-validate/rules';
 
 export default {
@@ -24,6 +26,28 @@ export default {
     defineRule('email', email);
     defineRule('min_value', minVal);
     defineRule('max_value', maxVal);
-    defineRule('confirmed', confirmed);
+    defineRule('password_missmatch', confirmed);
+    defineRule('excluded', excluded);
+    defineRule('country_excluded', excluded);
+
+    configure({
+      generateMessage: (ctx) => {
+        const messages = {
+          required: `The field ${ctx.field} required`,
+          min: `The field ${ctx.field} is too short`,
+          country_excluded: 'Do no except user from this location',
+          password_missmatch: 'The passwords not match',
+        };
+        const message = messages[ctx.rule.name]
+          ? messages[ctx.rule.name]
+          : `The field ${ctx.field} is not valid`;
+
+        return message;
+      },
+      validateOnBlur: true,
+      validateOnChange: true,
+      validateOnInput: false,
+      validateOnModelUpdate: true,
+    });
   },
 };
